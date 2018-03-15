81da87badfb64c96c2febbbeb109f7465993a065
ionix <ionixxx@gmail.com>
2018-03-15 20:59:36 +0300

    upload file

--- routers/upload.js
+++ routers/upload.js
@@ -3,7 +3,17 @@ const express = require('express')
 const router = express.Router()
 const multer = require('multer')
 const limits = {fileSize: 1024 * 1024 * 10}
-const upload = multer({dest: 'uploads/', limits: limits})
+
+const storage = multer.diskStorage({
+  destination: function (request, file, callback) {
+    callback(null, './uploads/')
+  },
+  filename: function (request, file, callback) {
+    callback(null, Date.now() + '-' + file.originalname)
+  }
+})
+
+const upload = multer({ storage: storage, limits: limits })
 
 router.post('/', upload.single('doc'), function (req, res, next) {
   if (req.file) {
