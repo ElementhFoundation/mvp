a507dd03c5d121019d71939d02bbb0e1d3cb4cd5
Ferrumz <ferrumz@gmail.com>
2018-03-15 21:03:02 +0300

    additional search

--- routers/search.js
+++ routers/search.js
@@ -5,64 +5,129 @@ const router = express.Router()
 
 router.get('/', async function (req, res) {
   try {
-    let q = req.query.q ? req.query.q : 'tyres'
-    let width = req.query.width ? req.query.width : null
-    let height = req.query.height ? req.query.height : null
-    let radius = req.query.radius ? req.query.radius : null
-    let season = req.query.season ? req.query.season : null
+    let q = req.query.q ? req.query.q : ''
+    let searchType = req.query['search-type']
 
+    if (req.query[searchType + '-brand'] && searchType !== 'none') q += ' ' + req.query[searchType + '-brand']
+    if (req.query[searchType + '-model'] && searchType !== 'none') q += ' ' + req.query[searchType + '-model']
     let filter = {bool: {must: []}}
 
-    if (width) {
-      filter.bool.must.push({
-        'term': {
-          'attributes.alias': 'width'
-        }
-      })
-      filter.bool.must.push({
-        'term': {
-          'attributes.value': width
-        }
-      })
-    }
+    switch (searchType) {
+      case 'none':
+        break
+      case 'wheels':
+        let wheelsWidth = req.query['wheels-width'] ? req.query['wheels-width'] : null
+        let wheelsRadius = req.query['wheels-radius'] ? req.query['wheels-radius'] : null
+        let wheelsDia = req.query['wheels-dia'] ? req.query['wheels-dia'] : null
 
-    if (height) {
-      filter.bool.must.push({
-        'term': {
-          'attributes.alias': 'height'
+        if (wheelsWidth) {
+          filter.bool.must.push({
+            'term': {
+              'attributes.alias': 'width'
+            }
+          })
+          filter.bool.must.push({
+            'term': {
+              'attributes.value': wheelsWidth
+            }
+          })
         }
-      })
-      filter.bool.must.push({
-        'term': {
-          'attributes.value': height
-        }
-      })
-    }
 
-    if (radius) {
-      filter.bool.must.push({
-        'term': {
-          'attributes.alias': 'radius'
+        if (wheelsRadius) {
+          filter.bool.must.push({
+            'term': {
+              'attributes.alias': 'radius'
+            }
+          })
+          filter.bool.must.push({
+            'term': {
+              'attributes.value': wheelsRadius
+            }
+          })
         }
-      })
-      filter.bool.must.push({
-        'term': {
-          'attributes.value': radius
-        }
-      })
-    }
 
-    if (season) {
-      filter.bool.must.push({
-        'term': {
-          'attributes.alias': 'season'
+        if (wheelsDia) {
+          filter.bool.must.push({
+            'term': {
+              'attributes.alias': 'dia'
+            }
+          })
+          filter.bool.must.push({
+            'term': {
+              'attributes.value': wheelsDia
+            }
+          })
         }
-      })
-      filter.bool.must.push({
-        'term': {
-          'attributes.value': season
+        filter.bool.must.push({
+          'term': {
+            'model.category.alias': 'wheels'
+          }
+        })
+        break
+      case 'tires':
+        let tiresWidth = req.query['tires-width'] ? req.query['tires-width'] : null
+        let tiresHeight = req.query['tires-height'] ? req.query['tires-height'] : null
+        let tiresRadius = req.query['tires-radius'] ? req.query['tires-radius'] : null
+        let tiresSeason = req.query['tires-season'] ? req.query['tires-season'] : null
+
+        if (tiresWidth) {
+          filter.bool.must.push({
+            'term': {
+              'attributes.alias': 'width'
+            }
+          })
+          filter.bool.must.push({
+            'term': {
+              'attributes.value': tiresWidth
+            }
+          })
         }
-      })
+
+        if (tiresHeight) {
+          filter.bool.must.push({
+            'term': {
+              'attributes.alias': 'height'
+            }
+          })
+          filter.bool.must.push({
+            'term': {
+              'attributes.value': tiresHeight
+            }
+          })
+        }
+
+        if (tiresRadius) {
+          filter.bool.must.push({
+            'term': {
+              'attributes.alias': 'radius'
+            }
+          })
+          filter.bool.must.push({
+            'term': {
+              'attributes.value': tiresRadius
+            }
+          })
+        }
+
+        if (tiresSeason) {
+          filter.bool.must.push({
+            'term': {
+              'attributes.alias': 'season'
+            }
+          })
+          filter.bool.must.push({
+            'term': {
+              'attributes.value': tiresSeason
+            }
+          })
+        }
+
+        filter.bool.must.push({
+          'term': {
+            'model.category.alias': 'tyres'
+          }
+        })
+        break
     }
 
     let query = {
