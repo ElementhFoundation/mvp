081838cc89f6b72edc3839919a29b47744927c41
ionix <ionixxx@gmail.com>
2018-03-15 19:49:28 +0300

    upload file

--- routers/upload.js
+++ routers/upload.js
@@ -2,13 +2,15 @@ const config = require('config')
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
-const limits = { fileSize: 1024 * 1024 * 10 }
+const limits = {fileSize: 1024 * 1024 * 10}
 const upload = multer({dest: 'uploads/', limits: limits})
 
-router.post('/', upload.single('document'), function (req, res, next) {
-  // req.file is the `avatar` file
-  // req.body will hold the text fields, if there were any
-  res.json({response: {result: true}})
+router.post('/', upload.single('doc'), function (req, res, next) {
+  if (req.file) {
+    res.json({response: {result: true}})
+  } else {
+    res.json({response: {result: false}})
+  }
 })
 
 module.exports = router
